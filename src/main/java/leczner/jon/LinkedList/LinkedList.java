618c3bb5f08560ba6a3994403be3c0603c2c1d41
Jonathan Leczner <jleczner@udel.edu>
2016-10-21 11:19:02 -0400

    set up stubs

---
+++ src/main/java/leczner/jon/LinkedList/LinkedList.java
@@ -0,0 +1,52 @@
+package leczner.jon.LinkedList;
+
+/**
+ * Created by jonathanleczner on 10/21/16.
+ */
+public class LinkedList {
+    Node head;
+
+    public LinkedList() {}
+
+    class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public void add(Node node) {
+        return;
+    }
+
+    public Node remove(int index) {
+        return null;
+    }
+
+    public boolean contains(Node list) {
+        return false;
+    }
+
+    public int find(Node list) {
+        return -1;
+    }
+
+    public int size(Node list) {
+        return -1;
+    }
+
+    public Node get(int index) {
+        return null;
+    }
+
+    public Node copy(Node list) {
+        return null;
+    }
+
+    public Node sort(Node list) {
+        return null;
+    }
+
+}
---
+++ src/test/java/leczner/jon/LinkedList/LinkedListTest.java
@@ -0,0 +1,49 @@
+package leczner.jon.LinkedList;
+
+import org.junit.Test;
+
+/**
+ * Created by jonathanleczner on 10/21/16.
+ */
+public class LinkedListTest {
+
+    @Test
+    public void addTest() {
+        LinkedList linkedList = new LinkedList();
+    }
+
+    @Test
+    public void removeTest() {
+
+    }
+
+    @Test
+    public void containsTest() {
+
+    }
+
+    @Test
+    public void findTest() {
+
+    }
+
+    @Test
+    public void sizeTest() {
+
+    }
+
+    @Test
+    public void getTest() {
+
+    }
+
+    @Test
+    public void copyTest() {
+
+    }
+
+    @Test
+    public void sortTest() {
+
+    }
+}
